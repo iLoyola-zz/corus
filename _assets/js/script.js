49436506d4ad9551a1241ef83a120fc26b9896f1
iLoyola <ivan@iloyola.com>
2016-08-23 23:10:52 -0400

    Configure CSS carousel, ajax call to external array, html index

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,22 +1,27 @@
-var corusURL = 'http://static.globalnews.ca/content/test/results-2011.js';
+var corusURL = 'https://static.globalnews.ca/content/test/results-2011.js';
+
+function gNews_getRidingDetailsCallback( data ) {
+  console.log( data.length );
+  var corusCardOutput = '';
+  for( var i = 0; i<data.length; i++ ){
+    corusCardOutput += '<input class="corus-open" type="radio" id="corus-' + data[i].num + '" name="corus" aria-hidden="true" hidden="" checked="checked"><div class="corus-item' + data[i].num + '"><div class="corus-card"><h1 class="corus-card-heading"></h1><ol class="corus-card-list"><li class="corus-card-list-item"><div class="corus-card-party"></div><div class="corus-card-candidate"></div><div class="corus-card-total"></div><div class="corus-card-percent"></div></li></ol></div></div>'
+    $( '.js-riding-card-container' ).append( corusCardOutput );
+    corusCardOutput = '';
+  }
+  $( '.corus-item1' ).attr( 'checked','checked' );
+}
 
 $( document ).ready( function(){
-  jQuery.cachedScript = function( url, options ) {
- 
-    // Allow user to set any option except for dataType, cache, and url
-    options = $.extend( options || {}, {
-      dataType: "script",
-      cache: true,
-      url: url
-    });
-   
-    // Use $.ajax() since it is more flexible than $.getScript
-    // Return the jqXHR object so we can chain callbacks
-    return jQuery.ajax( options );
-  };
-   
-  // Usage
-  $.cachedScript( corusURL ).done(function( script, textStatus ) {
-    console.log( textStatus );
+  $.ajax({
+    url: corusURL,
+    method: 'get',
+    dataType: 'script',
+    crossDomain: true,
+    success: function( data, textStatus ) {
+      console.log( textStatus );
+    },
+    fail: function( errMsg ) {
+      console.log( errMsg );
+    }
   });
 });
